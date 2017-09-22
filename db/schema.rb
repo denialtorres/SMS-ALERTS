d2d6746994852ffa079d6c9fb471de418bf36b6d
Gerardo Gaona <gerardo.gaona@gmail.com>
2017-09-22 16:58:05 -0500

    Update 'alerts' table

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,15 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170922072657) do
+ActiveRecord::Schema.define(version: 20170922213428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "alerts", force: :cascade do |t|
     t.string   "message"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "category_id"
+    t.float    "longitude"
+    t.float    "latitude"
+    t.index ["category_id"], name: "index_alerts_on_category_id", using: :btree
   end
 
   create_table "categories", force: :cascade do |t|
@@ -39,9 +42,9 @@ ActiveRecord::Schema.define(version: 20170922072657) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
-    t.integer  "phone",      limit: 8
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
+    t.bigint   "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string   "email"
     t.string   "about_yo"
     t.string   "estate"
