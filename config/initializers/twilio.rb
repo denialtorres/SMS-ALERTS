d625f819a6abd659901f7fbfe9f7f379a8d10884
denialtorres <denial.torres@gmail.com>
2017-09-22 00:01:25 +0000

    initializer

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -1,5 +1,6 @@
 # Configure Twilio
+#SON PUBLICAS PERO NO SE PASEN DE VERGAS, ES SOLO PARA PRUEBAS
 Twilio.configure do |config|
-  config.account_sid = ENV['TWILIO_SID']
-  config.auth_token = ENV['TWILIO_TOKEN']
+  config.account_sid = 'AC248cb2931b0eef49ffa9e240e27c4a70'
+  config.auth_token = '19d8e06c60d72ee8e8c744d9e8b5dbc9'
 end    
