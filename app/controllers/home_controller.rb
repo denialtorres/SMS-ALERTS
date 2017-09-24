fb9b9717ef8c52553e5b38c58a54e1890b4f0c23
Gerardo Gaona <gerardo.gaona@gmail.com>
2017-09-24 10:47:55 -0500

    Enable 'load more' feature on 'index'

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,17 @@
 class HomeController < ApplicationController
   def index
-    @contacts = Contact.all
-    @alerts = Alert.all.order('created_at ASC').reverse_order
+    # @contacts = Contact.all
+    @alerts    = Alert.page(page)
+    @next_page = Alert.next_page(page)
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  private
+  def page
+    (params[:page] || 1).to_i
   end
 end
--- app/models/alert.rb
+++ app/models/alert.rb
@@ -1,8 +1,19 @@
 class Alert < ActiveRecord::Base
+  default_scope { order(created_at: :desc) }
+
+  PAGE_SIZE = 6
+
   belongs_to :category
   has_many   :contacts, through: :category
 
   validates :category_id, presence: true
+
+  scope :page, -> (page) { offset((page - 1) * PAGE_SIZE).limit(PAGE_SIZE) }
+
+  def self.next_page(current_page)
+    next_page = current_page + 1
+    next_page if page(next_page).count > 0
+  end
 
   # deprecated, use `contacts` instead
   def subscribed_users
