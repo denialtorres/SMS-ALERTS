2d4f361d3544b12c07a2007f546a3ba0de2a2003
Gerardo Gaona <gerardo.gaona@gmail.com>
2017-09-25 12:36:28 -0500

    Remove unused and/or repeated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
  scope '/(:locale)', defaults: { locale: 'es' }, constraints: { locale: /en|es/ } do
-    get 'home/index'
-    
+    root "home#index"
     
     resources :alerts
-    resources :contacts,  :only => [:new, :create]
-   
-    match 'formulario' => 'contacts#new',  :via => [:get]
-
-   
-    root "home#index"
+    resources :contacts,  only: [:create]
+    get 'formulario' => 'contacts#new'
  end
 end
